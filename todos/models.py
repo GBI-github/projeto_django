5784da4cbf4bff0653f0f53da90ad062bb458d5b
Gabriel <jgga07000@gmail.com>
2023-12-15 20:44:23 -0300

    md3

--- todos/models.py
+++ todos/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+
 class Todo(models.Model):
-    title =  models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.TimeField(null=False, blank=False)
     finished_at = models.DateTimeField(null=True)
-
